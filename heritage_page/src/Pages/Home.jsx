26a34368161bcdc6e5b83c7def522ba23e35a67c
JUSTINA-00 <justinasam@karunya.edu.in>
2024-10-18 22:39:36 +0530

    added pictures to home page

--- heritage_page/src/Pages/Home.jsx
+++ heritage_page/src/Pages/Home.jsx
@@ -1,14 +1,47 @@
-import React from "react";
-import { useDisclosure } from '@chakra-ui/react';
-
+import React from 'react';
+import Slider from 'react-slick';
+import { Box, Image } from '@chakra-ui/react';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-  
-    return (
-        <h1>Home</h1>
-    );
+  // Carousel settings
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
   };
-  
-  export default Home;
-  
+
+  // List of images
+  const images = [
+    require('../Components/Pictures/1.png'),
+    require('../Components/Pictures/2.png'),
+    require('../Components/Pictures/3.png'),
+  ];
+
+  return (
+    <Box maxW="full" mx="auto" mt={5} p={5}>
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <Box key={index}>
+            <Image
+              src={image}
+              alt={`Slide ${index + 1}`}
+              objectFit="cover"
+              width="100%"
+              height="500px" // Adjust height as needed
+              borderRadius="md"
+            />
+          </Box>
+        ))}
+      </Slider>
+    </Box>
+  );
+};
+
+export default Home;
